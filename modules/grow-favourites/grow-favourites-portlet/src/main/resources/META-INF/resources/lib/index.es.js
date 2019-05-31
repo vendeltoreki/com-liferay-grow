bde069bba7df6c0661fb3238f104ddef334a841f
Roland Pákai <roland.pakai@liferay.com>
2019-05-31 12:17:26 +0200

    GROW-830 Add componentDidMount function

--- modules/grow-favourites/grow-favourites-portlet/src/main/resources/META-INF/resources/lib/index.es.js
+++ modules/grow-favourites/grow-favourites-portlet/src/main/resources/META-INF/resources/lib/index.es.js
@@ -6,18 +6,7 @@ import GrowFavouritesSlide from './modules/GrowFavouritesSlide.es';
 import GrowIcon from './modules/GrowIcon.es';
 
 const spritemap = Liferay.ThemeDisplay.getPathThemeImages();
-
-const GrowFavouriteCardData = {
-	articleAuthor: "Gábor Ambrózy",
-	authorAvatar: "/o/GrowFavouritesPortlet/images/0.jpeg",
-	createDate: "01.01.2019",
-	articleTitle: "Respect badge",
-	articleContent:
-		'A respect badge can be given by anyone to anyone. You can give 1 badge per month. You can use the Respect badge page to add badges. To give a respect badge, @ mention the name in the table, write your name to the From column and write the reason, why do you give it. The reason has to be for a "superpower", something why you respect the other person and something which you would like to learn from them. For Support Hungary 2016Q3 goals, you have to give min. 1 respect badge until Sept 30. If there will be need, we will add gamification to Grow later on.',
-	tags: ["badge", "gamification", "respect", "test1", "test2"],
-	readCount: "626",
-	articleCategory: "Share"
-};
+const favoruitesUrl = 'https://jsonplaceholder.typicode.com/todos/1';
 
 const mockupData = {
 	"data": [
@@ -110,9 +99,20 @@ class App extends React.Component {
 		super(props);
 		this.state = 
 			{
-				data: mockupData.data,
+				data: [],
 				spritemap: spritemap
 			};
+	}
+	
+	componentDidMount() {
+		fetch(favoruitesUrl)
+			.then(
+				/*response => response.json()*/
+				response => mockupData.data
+			)
+			.then(
+				data => this.setState({ data })
+			);
 	}
 	
 	render() {

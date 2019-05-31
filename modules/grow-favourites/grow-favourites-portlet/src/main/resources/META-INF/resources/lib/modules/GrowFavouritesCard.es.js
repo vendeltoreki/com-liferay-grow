ffe2e3aa8d83db08669ad2e7ed06b747954f28a3
Roland Pákai <roland.pakai@liferay.com>
2019-05-31 19:19:58 +0200

    GROW-830 SF

--- modules/grow-favourites/grow-favourites-portlet/src/main/resources/META-INF/resources/lib/modules/GrowFavouritesCard.es.js
+++ modules/grow-favourites/grow-favourites-portlet/src/main/resources/META-INF/resources/lib/modules/GrowFavouritesCard.es.js
@@ -3,103 +3,103 @@ import GrowIcon from "./GrowIcon.es";
 
 class GrowFavouritesCard extends React.Component {
 	
-  constructor(props) {
-	super(props);
+	constructor(props) {
+		super(props);
 	
-    this.state = {
-		star: this.props.star
-	};
+		this.state = {
+			star: this.props.star
+		};
 	
-    this.handleClick = this.handleClick.bind(this);
-  }
+		this.handleClick = this.handleClick.bind(this);
+	}
 
-  handleClick() {
-    this.setState(state => ({
-      star: !state.star
-    }));
-  }
+	handleClick() {
+		this.setState(state => ({
+		  star: !state.star
+		}));
+	}
 
-  render() {
-    return (
-      <div
-          className={"card card-" + this.props.articleCategory.toLowerCase() + " mr-2 ml-2"}
-        >
-        <div className="card-body">
-          <div className="autofit-row autofit-padded mb-2">
-            <div className="autofit-col">
-              <div className="autofit-section">
-                <img className="img-fluid sticker sticker-primary sticker-xl rounded-circle"
-                      src={this.props.articleAuthorAvatar} />
-              </div>
-            </div>
-          
-            <div className="autofit-col autofit-col-expand">
-              <div className="autofit-section text-secondary">
-                <strong>{this.props.articleAuthor}</strong>
-                <div className="break">{this.props.articleCreateDate}</div>
-              </div>
-            </div>
-            <div className="autofit-section">
-              <button className="btn btn-outline-secondary btn-outline-borderless" type="button" onClick={this.handleClick}>
-                  {this.state.star && (
-                    <GrowIcon
-                      spritemap={this.props.spritemap}
-                      classes="lexicon-icon inline-item"
-                      iconName="star"
-                    />
-                  )}
-                  {this.state.star == false && (
-                    <GrowIcon
-                    spritemap={this.props.spritemap}
-                    classes="lexicon-icon inline-item"
-                    iconName="star-o"
-                    />
-                  )}
-              </button>
-              <button className="btn btn-outline-secondary btn-outline-borderless" type="button">
-                    {(() => {
-                      switch(this.props.articleCategory) {
-                          case 'Excellence':
-                          return <GrowIcon
-                              spritemap={this.props.spritemap}
-                              classes="lexicon-icon inline-item inline-item-before icon-excellence"
-                              iconName="sheets"
-                          />;
-                          case 'Learn':
-                          return <GrowIcon
-                              spritemap={this.props.spritemap}
-                              classes="lexicon-icon inline-item inline-item-before icon-learn"
-                              iconName="info-book"
-                          />;
-                          case 'People':
-                          return <GrowIcon
-                              spritemap={this.props.spritemap}
-                              classes="lexicon-icon inline-item inline-item-before icon-people"
-                              iconName="user"
-                          />;
-                          default:
-                          return <GrowIcon
-                              spritemap={this.props.spritemap}
-                              classes="lexicon-icon inline-item inline-item-before icon-share"
-                              iconName="share"
-                          />;
-                      }
-                  })()}
-              </button>
-            </div>
-          </div>
+	render() {
+		return (
+			<div
+			  className={"card card-" + this.props.articleCategory.toLowerCase() + " mr-2 ml-2"}
+			>
+				<div className="card-body">
+				  <div className="autofit-row autofit-padded mb-2">
+					<div className="autofit-col">
+					  <div className="autofit-section">
+						<img className="img-fluid sticker sticker-primary sticker-xl rounded-circle"
+							  src={this.props.articleAuthorAvatar} />
+					  </div>
+					</div>
+				  
+					<div className="autofit-col autofit-col-expand">
+					  <div className="autofit-section text-secondary">
+						<strong>{this.props.articleAuthor}</strong>
+						<div className="break">{this.props.articleCreateDate}</div>
+					  </div>
+					</div>
+					<div className="autofit-section">
+					  <button className="btn btn-outline-secondary btn-outline-borderless" type="button" onClick={this.handleClick}>
+						  {this.state.star && (
+							<GrowIcon
+							  spritemap={this.props.spritemap}
+							  classes="lexicon-icon inline-item"
+							  iconName="star"
+							/>
+						  )}
+						  {this.state.star == false && (
+							<GrowIcon
+							spritemap={this.props.spritemap}
+							classes="lexicon-icon inline-item"
+							iconName="star-o"
+							/>
+						  )}
+					  </button>
+					  <button className="btn btn-outline-secondary btn-outline-borderless" type="button">
+							{(() => {
+							  switch(this.props.articleCategory) {
+								  case 'Excellence':
+								  return <GrowIcon
+									  spritemap={this.props.spritemap}
+									  classes="lexicon-icon inline-item inline-item-before icon-excellence"
+									  iconName="sheets"
+								  />;
+								  case 'Learn':
+								  return <GrowIcon
+									  spritemap={this.props.spritemap}
+									  classes="lexicon-icon inline-item inline-item-before icon-learn"
+									  iconName="info-book"
+								  />;
+								  case 'People':
+								  return <GrowIcon
+									  spritemap={this.props.spritemap}
+									  classes="lexicon-icon inline-item inline-item-before icon-people"
+									  iconName="user"
+								  />;
+								  default:
+								  return <GrowIcon
+									  spritemap={this.props.spritemap}
+									  classes="lexicon-icon inline-item inline-item-before icon-share"
+									  iconName="share"
+								  />;
+							  }
+						  })()}
+					  </button>
+					</div>
+				  </div>
 
-          <div className="autofit-row autofit-padded">
-            <div className="autofit-col autofit-col-expand">
-            <div className="autofit-section">
-                <h2>{this.props.articleTitle}</h2>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    )
-  }
+				  <div className="autofit-row autofit-padded">
+					<div className="autofit-col autofit-col-expand">
+					<div className="autofit-section">
+						<h2>{this.props.articleTitle}</h2>
+					  </div>
+					</div>
+				  </div>
+				</div>
+			</div>
+		)
+	}
 
 }
 
--- modules/grow-favourites/grow-favourites-portlet/src/main/resources/META-INF/resources/lib/modules/GrowFavouritesSlide.es.js
+++ modules/grow-favourites/grow-favourites-portlet/src/main/resources/META-INF/resources/lib/modules/GrowFavouritesSlide.es.js
@@ -1,31 +1,30 @@
 import React from 'react';
-import GrowIcon from "./GrowIcon.es";
+
 import GrowFavouritesCard from './GrowFavouritesCard.es';
 
 class GrowFavouritesSlide extends React.Component {
   
-  constructor(props) {
-    super(props);
-	
-	this.state = 
-	{
-		data: this.props.data,
-		slideIndex: this.props.slideIndex,
-		spritemap: this.props.spritemap
-	};
-  }
+	constructor(props) {
+		super(props);
+		
+		this.state = {
+			data: this.props.data
+		};
+	}
 
   render() {
+	  
+	const { data } = this.state; 
 	  
     return (
 		<div className="grow-favourites-slide">
 			
-			{this.state.data.map((cardData, key) => 
-				<div className="row" key={"row-"+this.state.slideIndex+"-"+key}>
-					<div className="col-sm" key={"col-"+this.state.slideIndex+"-"+key}>
+			{data.map((cardData, key) => 
+				<div className="row" key={"row-"+this.props.slideIndex+"-"+key}>
+					<div className="col-sm" key={"col-"+this.props.slideIndex+"-"+key}>
 						<GrowFavouritesCard
-							key={"growfavouritescard-"+this.state.slideIndex+"-"+key}
-							spritemap={this.state.spritemap}
+							key={"growfavouritescard-"+this.props.slideIndex+"-"+key}
+							spritemap={this.props.spritemap}
 							articleAuthor={cardData.articleAuthor}
 							articleAuthorAvatar={cardData.authorAvatar}
 							articleCreateDate={cardData.createDate}

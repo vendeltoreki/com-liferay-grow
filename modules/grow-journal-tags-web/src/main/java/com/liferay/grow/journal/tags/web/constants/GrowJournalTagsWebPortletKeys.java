ea70af6939025a1ac2a61794edb53080bf6a658e
balazssk <balazs.safrany-kovalik@liferay.com>
2020-09-07 14:54:49 +0200

    GROW-1268 creation of the MVC portlet

---
+++ modules/grow-journal-tags-web/src/main/java/com/liferay/grow/journal/tags/web/constants/GrowJournalTagsWebPortletKeys.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.grow.journal.tags.web.constants;
+
+/**
+ * @author Balázs Sáfrány-Kovalik
+ */
+public class GrowJournalTagsWebPortletKeys {
+
+	public static final String GROWJOURNALTAGSWEB =
+		"com_liferay_grow_journal_tags_web_GrowJournalTagsWebPortlet";
+
+}
